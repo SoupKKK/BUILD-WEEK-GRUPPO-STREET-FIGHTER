45b1e3ea1c02e953ef3e4eacd98d1e98c4d908f5
MatteoRomano7 <ezmatteo@aol.com>
2023-11-14 16:26:52 +0100

    migliore codici e JS

--- result.js
+++ result.js
@@ -1,11 +1,29 @@
+let rispostegiuste = 4;
+let rispostesbagliate = 10 - rispostegiuste;
 var xValues = ["RisposteGiuste", "RisposteSbagliate"];
-var yValues = [18, 7];
-var barColors = ["#b91d47", "#2b5797"];
+var yValues = [rispostegiuste, rispostesbagliate];
+var barColors = ["#00FFFF", " #D20094 "];
+
+function textCenter() {
+  const centerText = document.getElementById("centerText");
+
+  if (rispostegiuste >= 6) {
+    centerText.innerHTML +=
+      "<h3>Congratulations! <br><span>You passed the examination.</span></h3> ";
+  } else {
+    centerText.innerHTML +=
+      "<h3>I am sorry <br> <span>You didn't manage<br> to pass the examination.</span></h3>";
+  }
+  centerText.innerHTML +=
+    "<br><p>We'll send you the certificate<br>  in few minutes. <br> Check your email (including <br> promotions/spam folder)</p>";
+}
+
+textCenter();
 
 new Chart("myChart", {
   type: "doughnut",
   data: {
-    labels: xValues,
+    labels: ["Corrette", "Errate"],
     datasets: [
       {
         backgroundColor: barColors,
@@ -16,8 +34,17 @@ new Chart("myChart", {
   options: {
     title: {
       display: true,
-      text: "World Wide Wine Production 2018",
+      position: "bottom",
+      fontSize: 20,
+      fontColor: "white",
+      fontStyle: "bold",
     },
-    cutoutPercentage: 75, // Puoi regolare questo valore per controllare la dimensione del taglio centrale
+    cutoutPercentage: 75,
+    legend: {
+      display: false,
+    },
+    tooltips: {
+      enabled: false,
+    },
   },
 });
